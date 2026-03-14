95944dd16f9cf309816bd7b98fcfae1017f956a4
astotux <itzartiktemka@gmail.com>
2026-03-14 10:30:32 +0300

    fix favicon for yandex 2

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/favicon.ico',
+        headers: [
+          { key: 'Content-Type', value: 'image/x-icon' },
+          { key: 'Cache-Control', value: 'public, max-age=86400' },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
