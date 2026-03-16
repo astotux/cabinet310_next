de20ed86f91dbb0525bd986fa5b89d391cba1159
astotux <itzartiktemka@gmail.com>
2026-03-16 09:30:23 +0300

    edit salon

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Кабинет 310 — Студия красоты в Сыктывкаре',
+    name: 'Кабинет 310 — Салон красоты в Сыктывкаре',
     short_name: 'Кабинет 310',
     description: 'Перманентный макияж бровей, губ и межреснички, маникюр, ламинирование ресниц в Сыктывкаре',
     start_url: '/',
